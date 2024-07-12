8749417a6681d4b85a3a1e00fbd20414ca717653
Hai Nguyen <nvhai245@gmail.com>
2024-07-12 11:06:01 +0700

    updated dmg-builder version

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
       nodeIntegration: true,
       customFileProtocol: './',
       builderOptions: {
+        asar: true,
         appId: "weektodo-app.netlify.app",
         productName: "WeekToDo",
         publish: ["github"],
@@ -24,3 +25,19 @@ module.exports = {
     },
   }
 };
+
+const crypto = require('crypto');
+
+/**
+ * The MD4 algorithm is not available anymore in Node.js 17+ (because of library SSL 3).
+ * In that case, silently replace MD4 by the MD5 algorithm.
+ */
+try {
+  crypto.createHash('md4');
+} catch (e) {
+  console.warn('Crypto "MD4" is not supported anymore by this Node.js version');
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
+  };
+}
